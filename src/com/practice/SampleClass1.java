be6c12c1952f219570496a32c1ae34441bce195a
creed0607 <agaur968@gmail.com>
2021-10-07 12:28:30 +0530

    Initial commit

---
+++ src/com/practice/SampleClass1.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+public class SampleClass1 {
+    public static void main(String[] args){
+
+        System.out.println("First line of code");
+    }
+}
---
+++ src/com/practice/SampleClass2.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+public class SampleClass2 {
+    public static void main(String[] args){
+
+        System.out.println("First line of code of SampleClass 2");
+    }
+}
