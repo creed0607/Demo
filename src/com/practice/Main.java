3a92c7efb7b6b2140e91b53bbc5b2a27f4e8d3bf
creed0607 <agaur968@gmail.com>
2022-01-04 13:08:26 +0530

    Next commit

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("First code in Intellij ide");
+
+System.out.println("Second line in Intellij ide");
+System.out.println("third Line in Intellij ide");
     }
+
 }
